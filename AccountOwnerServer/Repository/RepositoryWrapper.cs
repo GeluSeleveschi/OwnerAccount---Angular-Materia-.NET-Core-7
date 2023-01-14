3ea45303856d5ec97f3289a6db8926f0880ef154
gelu@codeindustry.net <gelu@codeindustry.net>
2023-01-14 22:50:25 +0200

    updated the backend part

---
+++ AccountOwnerServer/Repository/RepositoryWrapper.cs
@@ -0,0 +1,48 @@
+﻿using Contracts;
+using Entities;
+
+namespace Repository
+{
+    public class RepositoryWrapper: IRepositoryWrapper
+    {
+        private RepositoryContext _repositoryContext;
+        private IOwnerRepository _ownerRepo;
+        private IAccountRepository _accountRepo;
+
+        public IOwnerRepository Owner
+        {
+            get
+            {
+                if (_ownerRepo == null)
+                {
+                    _ownerRepo = new OwnerRepository(_repositoryContext);
+                }
+
+                return _ownerRepo;
+            }
+        }
+
+        public IAccountRepository Account
+        {
+            get
+            {
+                if (_accountRepo == null)
+                {
+                    _accountRepo = new AccountRepository(_repositoryContext);
+                }
+
+                return _accountRepo;
+            }
+        }
+
+        public RepositoryWrapper(RepositoryContext repositoryContext)
+        {
+            _repositoryContext = repositoryContext;
+        }
+
+        public void Save()
+        {
+            _repositoryContext.SaveChanges();
+        }
+    }
+}
