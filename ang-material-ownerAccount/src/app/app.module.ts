61398d340ecdde414cc9b641cc853efbb14e3acc
gelu@codeindustry.net <gelu@codeindustry.net>
2023-02-19 17:48:49 +0200

    updated angular app

--- ang-material-ownerAccount/src/app/app.module.ts
+++ ang-material-ownerAccount/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { LayoutComponent } from './layout/layout.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { HomeComponent } from './home/home.component';
 import { RoutingModule } from './routing/routing.module';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
@@ -17,13 +16,11 @@ import { NotFoundComponent } from './error-pages/not-found/not-found.component';
 import { ServerErrorComponent } from './error-pages/server-error/server-error.component';
 import { DatePipe } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
-    HomeComponent,
     HeaderComponent,
     SidenavListComponent,
     NotFoundComponent,
--- ang-material-ownerAccount/src/app/routing/routing.module.ts
+++ ang-material-ownerAccount/src/app/routing/routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from '../home/home.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from '../error-pages/not-found/not-found.component';
 import { ServerErrorComponent } from '../error-pages/server-error/server-error.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
   { path: 'owner', loadChildren: () => import('./../owner/owner.module').then(m => m.OwnerModule) },
   { path: '404', component: NotFoundComponent },
   { path: '500', component: ServerErrorComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/owner/owners', pathMatch: 'full' },
   { path: '**', redirectTo: '/404', pathMatch: 'full' },
 ]
 
